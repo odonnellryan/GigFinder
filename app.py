47b34689d59bcf1f93e7e6efe5f9c35a5157a1b0
Ryan O'Donnell <odonnellryanc@gmail.com>
2016-02-15 22:44:59 -0500

    Added DB layer

--- app.py
+++ app.py
@@ -2,20 +2,23 @@ import flask
 import requests
 from urllib import parse
 from bs4 import BeautifulSoup
+from db import database, Gigs
 
 locations = ['newjersey']
 
 def build_craigslist_data_object(soup, url, location):
     rows = soup.find('div', class_='content').find('span', class_='rows').find_all('p')
-    data = {}
+    data = []
     for row in rows:
         town = row.find('span', class_='pnr').find('small')
         _location = " ".join([location, town.text.strip()]) if town else location
-        data[row['data-pid']] = {
+        data.append({
+            'website_supplied_id': row['data-pid'],
             'name': row.find('a', class_='hdrlnk').text.strip(),
             'url': parse.urljoin(url, row.a['href']),
-            'location': _location
-        }
+            'location': _location,
+            'datetime': row.find('time')['datetime']
+        })
     return data
 
 def craigslist_searcher(locations):
@@ -24,7 +27,14 @@ def craigslist_searcher(locations):
         url = "https://{}.craigslist.org/search/cpg/".format(location)
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'lxml')
-        data.append(build_craigslist_data_object(soup, url, location))
+        data.extend(build_craigslist_data_object(soup, url, location))
     return data
 
-print(craigslist_searcher(locations))
+def insert_into_db(data):
+    with database.atomic():
+        for item in data:
+            Gigs.create_or_get(website_supplied_id=item['website_supplied_id'], name=item['name'], url=item['url'],
+                               location=item['location'], datetime=item['datetime'])
+
+
+print(insert_into_db(craigslist_searcher(locations)))
---
+++ db.py
@@ -0,0 +1,14 @@
+from peewee import SqliteDatabase, Model, IntegerField, TextField, DateTimeField
+
+database = SqliteDatabase('gigFinder.db')
+
+class GigFinder(Model):
+    class Meta:
+        database = database
+
+class Gigs(GigFinder):
+    website_supplied_id = TextField
+    name = TextField(null=True, default=None)
+    url = TextField(null=True)
+    location = TextField(null=True)
+    datetime = DateTimeField(null=True)
